03287d1062bd6187c64241a78f37767edfc70757
Urs Müller <ursmueller@gmx.ch>
2025-08-31 10:10:58 +0200

    Update for chapter 4, databases

--- microblog.py
+++ microblog.py
@@ -1,4 +1,12 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
 
 if __name__ == "__main__":
     app.run()
+
+
+@app.shell_context_processor
+def make_shell_context() -> dict:
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
